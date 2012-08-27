65f34e685a9416699ca7e90042907fb9187d374d
Etienne Tourigny <etourigny.dev@gmail.com>
2012-08-27 20:10:15 -0300

    mark 0.4.3 version

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ def description():
   return 'Display in a table or plot the values from the visible raster layers at the current mouse position' 
 
 def version(): 
-  return 'Version 0.4.2'
+  return 'Version 0.4.3'
 
 def qgisMinimumVersion():
   return '1.0'
